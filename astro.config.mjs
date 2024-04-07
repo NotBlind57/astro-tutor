15993e4adf5ec04be4784497590ca0e485620685
Me <me@me.me>
2024-04-07 20:05:49 +0400

    final plus swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gleaming-entremet-2df9e2.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), swup()]
 });
