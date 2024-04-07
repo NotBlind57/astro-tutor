f39081a346407644dcf9ed960743c606487db114
Me <me@me.me>
2024-04-07 17:31:12 +0400

    main is done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gleaming-entremet-2df9e2.netlify.app/"
+  site: "https://gleaming-entremet-2df9e2.netlify.app/",
+  integrations: [preact()]
 });
